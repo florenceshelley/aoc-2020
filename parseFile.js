59c96cc8ae272a1b72ca5670d94686ea5cf02819
Florence Shelley <smc.florence@gmail.com>
2020-12-02 11:20:48 -0500

    Restructure files and folders

--- parseFile.js
+++ parseFile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const parseFile = (file, callback) => {
-  if (!file) throw new Error('You must pass in a file to parse, e.g. `node executable.js file`');
+  if (!file) throw new Error('You must pass in a file to parse, i.e. `node executable.js file`');
 
   fs.readFile(file, 'utf-8', (err, data) => {
     if (err) throw new Error(err);
